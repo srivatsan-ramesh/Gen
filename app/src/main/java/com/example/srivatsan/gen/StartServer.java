b4b2a636dde890b4107f47e0a90d905caa0e08af
srivatsansoft <sriramesh4@gmail.com>
2015-05-09 20:41:07 +0530

    Integrating server part

--- app/src/main/java/com/example/srivatsan/gen/StartServer.java
+++ app/src/main/java/com/example/srivatsan/gen/StartServer.java
@@ -1,12 +1,18 @@
 package com.example.srivatsan.gen;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.srivatsan.gen.util.HTTPD;
-import com.example.srivatsan.gen.util.NanoHTTPD;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class StartServer extends ActionBarActivity {
@@ -16,8 +22,12 @@ public class StartServer extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_server);
 
+        String str = "<img src=\"Screenshot_2015-04-26-16-24-06.png\"></img>";
+        appendContent(str.getBytes());
+
         try {
-            HTTPD mServer = new HTTPD();
+            HTTPD fileRendering = new HTTPD(getApplicationContext(), 8080);
+            Log.i("server", "started");
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -25,6 +35,26 @@ public class StartServer extends ActionBarActivity {
 
     }
 
+    private void appendContent(byte[] text){
+
+        File f1 = new File(Environment.getExternalStorageDirectory().getPath(),"Gen");
+        f1.mkdirs();
+        Log.i("f1",String.valueOf(f1));
+        File f = new File(f1.getPath()+File.separator+"index.html");
+        try {
+            FileOutputStream fout = new FileOutputStream(f,true);
+            fout.write(text);
+            fout.flush();
+            fout.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.i("Empty File", "Not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/srivatsan/gen/util/HTTPD.java
+++ app/src/main/java/com/example/srivatsan/gen/util/HTTPD.java
@@ -1,14 +1,12 @@
 package com.example.srivatsan.gen.util;
 
-import android.net.wifi.WifiConfiguration;
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Method;
-import java.util.Map;
 import java.util.Properties;
-
-import javax.net.ssl.SSLEngineResult;
 
 /**
  * Created by aravind on 9/5/15.
@@ -18,16 +16,28 @@ public class HTTPD extends com.example.srivatsan.gen.util.NanoHTTPD {
     /**
      * Constructs an HTTP server on given port.
      */
-    public HTTPD() throws IOException {
-        super(8080);
+    Context mContext;
+    public HTTPD(Context mContext, int port) throws IOException {
+        super(port, Environment.getExternalStorageDirectory());
+        this.mContext = mContext;
     }
 
     @Override
     public Response serve(String uri, String method, Properties header, Properties params, Properties files, InputStream body)
     {
-        String msg = "";
-        msg += "<b>Sugan rocks</b>";
-        return new com.example.srivatsan.gen.util.NanoHTTPD.Response("200", "text/html", msg);
+        Log.i("uri",uri);
+        Log.i("method", method);
+        Log.i("params", params.toString());
+        if(uri.equals("/")) {
+            String msg = "";
+            msg += "<b>Sugan rocks</b>\n";
+            msg += "<img src=\"file:///sdcard/Screenshot_2015-04-26-16-24-06.png\"></img>";
+            return new com.example.srivatsan.gen.util.NanoHTTPD.Response("200", "text/html", msg);
+        }
+        else {
+
+        }
+        return new com.example.srivatsan.gen.util.NanoHTTPD.Response("200", "text/html", "none of these");
     }
 
 }
