c2957b9b387f07de669316f5b8705d1262e2a1fb
srivatsansoft <sriramesh4@gmail.com>
2015-05-09 21:11:20 +0530

    Working on layout changes

--- app/src/main/java/com/example/srivatsan/gen/MainActivity.java
+++ app/src/main/java/com/example/srivatsan/gen/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,6 +19,8 @@ public class MainActivity extends ActionBarActivity {
     private ClipboardManager myClipboard;
     private ClipData myClip;
     private EditText copyField,pasteField;
+    WebView webView = (WebView) findViewById(R.id.webview);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +30,28 @@ public class MainActivity extends ActionBarActivity {
         pasteField = (EditText)findViewById(R.id.paste);
     }
 
-
+    public void initClipboardData() {
+        paste();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        initClipboardData();
+    }
     public void copy(View view){
+        myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
         String text = copyField.getText().toString();
         myClip = ClipData.newPlainText("text", text);
         myClipboard.setPrimaryClip(myClip);
         Toast.makeText(getApplicationContext(), "Text Copied",
                 Toast.LENGTH_SHORT).show();
     }
-    public void paste(View view){
+    public void paste(){
+        myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
         ClipData abc = myClipboard.getPrimaryClip();
         ClipData.Item item = abc.getItemAt(0);
         String text = item.getText().toString();
