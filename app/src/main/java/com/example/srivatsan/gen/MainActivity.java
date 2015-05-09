9ecf6ec2e0b46b559a62fb13ede18c301ec0888c
srivatsansoft <sriramesh4@gmail.com>
2015-05-09 20:55:23 +0530

    Done with basic server part

--- app/src/main/java/com/example/srivatsan/gen/MainActivity.java
+++ app/src/main/java/com/example/srivatsan/gen/MainActivity.java
@@ -1,26 +1,55 @@
 package com.example.srivatsan.gen;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private ClipboardManager myClipboard;
+    private ClipData myClip;
+    private EditText copyField,pasteField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+        copyField = (EditText)findViewById(R.id.copy);
+        pasteField = (EditText)findViewById(R.id.paste);
     }
 
 
+    public void copy(View view){
+        String text = copyField.getText().toString();
+        myClip = ClipData.newPlainText("text", text);
+        myClipboard.setPrimaryClip(myClip);
+        Toast.makeText(getApplicationContext(), "Text Copied",
+                Toast.LENGTH_SHORT).show();
+    }
+    public void paste(View view){
+        ClipData abc = myClipboard.getPrimaryClip();
+        ClipData.Item item = abc.getItemAt(0);
+        String text = item.getText().toString();
+        pasteField.setText(text);
+        Toast.makeText(getApplicationContext(), "Text Pasted",
+                Toast.LENGTH_SHORT).show();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -29,10 +58,12 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/example/srivatsan/gen/util/HTTPD.java
+++ app/src/main/java/com/example/srivatsan/gen/util/HTTPD.java
@@ -2,11 +2,8 @@ package com.example.srivatsan.gen.util;
 
 import android.content.Context;
 import android.os.Environment;
-import android.util.Log;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 /**
  * Created by aravind on 9/5/15.
@@ -21,23 +18,4 @@ public class HTTPD extends com.example.srivatsan.gen.util.NanoHTTPD {
         super(port, Environment.getExternalStorageDirectory());
         this.mContext = mContext;
     }
-
-    @Override
-    public Response serve(String uri, String method, Properties header, Properties params, Properties files, InputStream body)
-    {
-        Log.i("uri",uri);
-        Log.i("method", method);
-        Log.i("params", params.toString());
-        if(uri.equals("/")) {
-            String msg = "";
-            msg += "<b>Sugan rocks</b>\n";
-            msg += "<img src=\"file:///sdcard/Screenshot_2015-04-26-16-24-06.png\"></img>";
-            return new com.example.srivatsan.gen.util.NanoHTTPD.Response("200", "text/html", msg);
-        }
-        else {
-
-        }
-        return new com.example.srivatsan.gen.util.NanoHTTPD.Response("200", "text/html", "none of these");
-    }
-
 }
